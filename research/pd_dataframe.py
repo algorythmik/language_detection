852b09248b2457ec08389f0c615e8e8d2259e68a
Mojtaba Tabatabaeipour <smt.moji@gmail.com>
2020-12-21 01:16:15 +0100

    clean dataframe.py

--- research/pd_dataframe.py
+++ research/pd_dataframe.py
@@ -12,16 +12,6 @@ def process_sentence(sentence):
     return re.sub(r'[\\\\/:*«`\'?¿";!<>,.|]', "", sentence.lower().strip())
 
 
-# def split_into_sentences(file_opj):
-
-#         for line in file_opj:
-#             splits = line.split(".")
-#             if len(splits) > 1:
-#                 sentence, remainder = splits[0], splits[1:]
-#             else:
-#                 reminder = splits
-
-
 sent_tokenizer = {
     "da": nltk.data.load("tokenizers/punkt/danish.pickle"),
     "no": nltk.data.load("tokenizers/punkt/danish.pickle"),
@@ -41,8 +31,6 @@ for lan in ["da", "no"]:
     dfs.append(df_lan)
 
 df = pd.concat(dfs)
-# del dfs
-# del df_lan
 df["text"] = df["text"].apply(process_sentence)
 
 df.to_pickle(os.path.join("data", "processed", "df_all.pkl"))
